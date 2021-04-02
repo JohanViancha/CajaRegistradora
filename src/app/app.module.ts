2ccb81b11458a9af81bec912b34d19df39d9de06
Johan Viancha <vianchajohan@gmail.com>
2021-04-01 19:58:18 -0500

    Modificación estructura de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './components/productos/productos.component';
+import { CajaComponent } from './components/caja/caja.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent
+    ProductosComponent,
+    CajaComponent
   ],
   imports: [
     BrowserModule
